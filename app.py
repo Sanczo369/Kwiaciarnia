9fd4c7e001c9a7c0589f20cfb5542a49e110a810
Arek Sanecki <areksanecki@gmail.com>
2023-10-01 21:08:20 +0200

    dodanie flask_wtf oraz klas formularzy

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 from flask import Flask
+from flask_wtf import FlaskForm 
+from wtforms import StringField, EmailField, TelField, PasswordField, TextAreaField
 app = Flask(__name__)
 
+
+class RegistrationForm(FlaskForm):
+    name = StringField('Imię', render_kw={"placeholder": "Imię"})
+    surname = StringField('Nazwisko', render_kw={"placeholder": "Nazwisko"})
+    email = EmailField('E-mail', render_kw={"placeholder": "E-mail"})
+    phone = TelField('Telefon', render_kw={"placeholder": "Telefon"})
+    password = PasswordField('Tekst', render_kw={"placeholder": "text"})
+
+class LoginForm(FlaskForm):
+    email = EmailField('E-mail', render_kw={"placeholder": "E-mail"})
+    password = PasswordField('Tekst', render_kw={"placeholder": "******"})
+    
+class NewsletterForm(FlaskForm):
+    email = EmailField('E-mail', render_kw={"placeholder": "E-mail"}) 
+    
+class ContactForm(FlaskForm):
+    name = StringField('Imię', render_kw={"placeholder": "Imię"})
+    email = EmailField('E-mail', render_kw={"placeholder": "E-mail"})
+    phone = TelField('Telefon', render_kw={"placeholder": "Telefon"})
+    password = TextAreaField('Tekst', render_kw={"placeholder": "..."})  
 @app.route('/') 
 def index(): 
     return '<h1>Hello World!!!</h1>' 
