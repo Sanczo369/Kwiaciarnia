f1bdbe08e32add4f8ff33c49a031e3a97901f9be
Arek Sanecki <areksanecki@gmail.com>
2023-10-03 20:46:18 +0200

    dodanie __init__.py

---
+++ shop/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_wtf import FlaskForm 
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+
+app.config.from_pyfile('config.cfg')
+db = SQLAlchemy(app)
+from shop import routes
