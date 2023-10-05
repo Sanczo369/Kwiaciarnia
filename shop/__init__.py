31c73befca9cafb0d27711c605bc3f9c21cb61c2
Arek Sanecki <areksanecki@gmail.com>
2023-10-05 13:51:35 +0200

    naprawienie błędów podczas uchuramiania register

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from flask_wtf import FlaskForm 
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from shop import routes
+
 app = Flask(__name__)
 
 
 app.config.from_pyfile('config.cfg')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+from shop.admin import routes
