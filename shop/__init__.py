da46c3dbca1d4947390ec30275d272e705c960ce
Arek Sanecki <areksanecki@gmail.com>
2023-10-05 13:29:05 +0200

    reorganizacja projektu

--- shop/__init__.py
+++ shop/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_wtf import FlaskForm 
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from shop import routes
 app = Flask(__name__)
 
 
 app.config.from_pyfile('config.cfg')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-from shop import routes
--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -11,6 +11,8 @@ def home():
 def register():
     form = RegistrationForm(request.form)
     if request.method == "POST" and form.validate():
+        # user = User(form.username.data, form.email.data, form.password.data)
+        # db_session.add(user)
         flash('Thanks for registering')
         return redirect(url_for('login'))
     return render_template('admin/register.html', form=form, title="Strona Rejestracji")
