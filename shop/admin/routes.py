cd67f11d1ee4437017dc1f17fc24a2b48731ed9b
Arek Sanecki <areksanecki@gmail.com>
2023-10-05 15:08:13 +0200

    dodanie hash_password z bcrypt

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -11,8 +11,9 @@ def home():
 def register():
     form = RegistrationForm(request.form)
     if request.method == "POST" and form.validate():
-        # user = User(form.username.data, form.email.data, form.password.data)
-        # db_session.add(user)
+        hash_password=bcrypt.generate_password_hash(form.password.data)
+        user = User(name=form.name.data, username=form.username.data, email=form.email.data, password=hash_password)
+        db.session.add(user)
         flash('Thanks for registering')
         return redirect(url_for('login'))
     return render_template('admin/register.html', form=form, title="Strona Rejestracji")
